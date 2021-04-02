fb510c6b2fa2e5b458838861dd830c0989040c41
Kidpaddleetcie <37848836+Kidpaddleetcie@users.noreply.github.com>
2021-04-02 21:24:05 +0200

    Update FlipnoteGlinCSS.js

--- FlipnoteGlinCSS.js
+++ FlipnoteGlinCSS.js
@@ -6,25 +6,41 @@
 // @author       Glin Kidpaddleetcie
 // @grant        none
 // @connect     sudomemo.net
+// @connect     raw.githubusercontent.com
 // @run-at      document-end
 // @include     http://*
 // @include     https://*
 // @updateURL   https://raw.githubusercontent.com/Kidpaddleetcie/FlipnoteGlinCSS/main/FlipnoteGlinCSS.js
 // @downloadURL https://raw.githubusercontent.com/Kidpaddleetcie/FlipnoteGlinCSS/main/FlipnoteGlinCSS.js
+// @grant       GM_listValues
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_deleteValue
+// @grant       GM_xmlhttpRequest
+// @grant       GM_info
+// @grant       GM_openInTab
+// @grant       GM_setClipboard
+// @grant       GM_registerMenuCommand
+// @grant       GM_unregisterMenuCommand
+// @grant       GM_notification
+// @grant       GM_download
+// @grant       GM.info
+// @grant       GM.listValues
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @grant       GM.deleteValue
+// @grant       GM.openInTab
+// @grant       GM.setClipboard
+// @grant       GM.xmlHttpRequest
+// @connect     *
 // ==/UserScript==
 
 (function() {
     'use strict';
     let tspliteuh = document.URL.split('/');
-    const elements = document.getElementsByTagName("link").rel("stylesheet");
+    const elements = document.getElementsByTagName("link");
     if (tspliteuh[2] === "www.sudomemo.net") {
-        for (let ii = 0; ii < elements.length; ii++) {
-            if (elements[ii].href === '/theatre_assets/css/main.css?ver=20210402_01') {
-                console.log(elements[ii]);
-                elements[ii].href = 'https://raw.githubusercontent.com/Kidpaddleetcie/FlipnoteGlinCSS/main/FlipnoteGlinCSS.css';
-            }
-        }
-
+        document.head.innerHTML = '<link rel="stylesheet" href="https://raw.githubusercontent.com/Kidpaddleetcie/FlipnoteGlinCSS/main/FlipnoteGlinCSS.css">';
     }
 
 })();
